9a2a20d68a4617391385541d020b2b13bf622ea4
Cedric Champeau <cedric.champeau@gmail.com>
2011-11-16 16:33:32 +0100

    Less groovy version but faster

--- check.groovy
+++ check.groovy
@@ -8,49 +8,38 @@
  */
 
 /**
- * Implement the "putAt" method on StringBuilder so that the syntax
- * stringbuilder[index] = 'X' delegates to setCharAt
- */
-StringBuilder.metaClass.putAt = { index, obj ->
-    delegate.setCharAt(index, (char) obj)
-}
-
-/**
- * Converts a string consisting of digits into a list of integers
- */
-def toIntList(str) { str.toCharArray().collect {it - 48} }
-
-/**
- * Implements the Luhn test on a list of integers
+ * Implements the Luhn test on a string of digits
  * @param digits
  * @return
  */
-def checkLuhn(digits) {
+def checkLuhn = { digits ->
     // the inject method is a "reduce" operation
     // we use a tuple as parameter which has as the first
     // element a boolean telling if we are on the second element of a pair, and second parameter the current sum
     digits.reverse().inject([false, 0]) { res, u ->
         def (right, sum) = res
+        u = ((char) u)-48
         sum += (right?(2 * u) % 10 + ((int) u / 5):u)
         [!right, sum]
     }[1] % 10 == 0
-}
+}.memoizeAtMost(10)
 
 /**
  * Implements the filtering of a single line
  */
-void filterLine(line) {
+def filterLine = { line ->
     // shared stringbuilder instance for every potential match on a line
     def sb = new StringBuilder()
     println line.replaceAll(/([0-9 -]+)/) { full, match ->
-        sb.setLength(0)
-        sb.append(match)
-        def cand = match.replaceAll(/[- ]/, '')
-        int len = cand.length()
-        def digits = toIntList(cand)
+        char x = 'X'
+        sb.length = 0
+        sb << match
+        def digits = match.replaceAll(/[- ]/, '')
+        int len = digits.length()
         int offset = 0
+        boolean found
         while (offset <= len - 14) {
-            boolean found = false
+            found = false
             for (int k = 16; k >= 14 && !found; k--) {
                 int matchOffset = offset
                 // sublist operator
@@ -58,15 +47,16 @@ void filterLine(line) {
 
                 // a credit card number is found if contains enough digits
                 // and passes the Luhn test
-                found = test.size() >= 14 && checkLuhn(test)
+                int size = test.size()
+                found = size >= 14 && checkLuhn(test)
                 if (found) {
                     // replace as many digits as found in the Luhn test
                     int rep = 0
-                    while (rep < test.size()) {
-                        char c = match[matchOffset]
+                    while (rep < size) {
+                        char c = match.charAt(matchOffset)
                         if (c.isDigit()) {
                             rep++
-                            sb[matchOffset] = 'X'
+                            sb.setCharAt(matchOffset, x)
                         }
                         matchOffset++
                     }
@@ -81,6 +71,6 @@ void filterLine(line) {
 }
 
 // main loop
-System.in.getText('US-ASCII').split(/\n/).each {
+System.in.eachLine('US-ASCII') {
     filterLine(it)
 }
